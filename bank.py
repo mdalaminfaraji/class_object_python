b0d718a99711d2f28476370bd6abc99bdaa7df4d
md. Alamin Faraji <alaminice1617@gmail.com>
2023-10-03 21:31:57 +0600

    bank oop

---
+++ bank.py
@@ -0,0 +1,22 @@
+class Bank:
+    def __init__(self, balance):
+        self.balance=balance
+        self.min_withdraw = 100
+        self.max_withdraw = 100000
+    def get_balance(self):
+        print(self.balance) 
+    def deposit(self, amount):
+        if amount>0:
+            self.balance +=amount
+    
+    def withdraw(self, amount):
+        if amount<self.min_withdraw:
+            print(f'forkira , you can withdraw below {self.min_withdraw}')
+        elif amount>self.max_withdraw:
+            print(f'bank fokir hoye jabe. you can not with more than {self.max_withdraw}') 
+        else:
+            self.balance -=amount
+            print(f'Here is your  money {amount}') 
+
+brac= Bank(23420)
+brac.withdraw(24)
--- shopInstance.py
+++ shopInstance.py
@@ -1,7 +1,7 @@
 class Shop:
     def __init__(self, buyer):
         self.buyer=buyer
-        self.cart=[]
+        self.cart=[]#instance attribute
     def add_to_cart(self, item):
         self.cart.append(item)
 
@@ -12,5 +12,5 @@ ahmadulilah.add_to_cart(" pad")
 
 alamin=Shop('alamin')
 alamin.add_to_cart('pen')
-print(ahmadulilah.cart)
+print(ahmadulilah.cart) 
 print(alamin.cart)
